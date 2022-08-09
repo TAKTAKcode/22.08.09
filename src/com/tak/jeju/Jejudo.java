4d060bf3d9a1747ae7eb3bd45ea00b284778fffb
tak <wontaek22@gmail.com>
2022-08-09 21:06:31 +0900

    22.08.09
    제주도 여행 첫번째 날..
    서울은 비가 엄청 많이 오고 굉장히 난리인데
    제주도는 해가 쨍쨍, 너무 덥다.
    까매질 것 같다.

---
+++ src/com/tak/jeju/Jejudo.java
@@ -0,0 +1,10 @@
+package com.tak.jeju;
+
+public class Jejudo {
+
+    public static void main(String[] args) {
+
+        System.out.println("제주도 놀러왔다. 오늘 비록 공부를 하지는 못했지만 자아성찰을 하다가 가야겠다. ^_^7");
+    }
+
+}
